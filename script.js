6bf34e8386aa81c6b9e4bf2c90552c112dabf900
tewodrosseble <tedseble77@gmail.com>
2024-10-30 18:55:30 +0300

    commit 2

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     const heading = document.createElement('h1');
-//     heading.textContent = 'Welcome to My Webpage!';
-//     document.body.appendChild(heading);
-
-//     const paragraph = document.createElement('p');
-//     paragraph.textContent = 'This is a simple script for a webpage.';
-//     document.body.appendChild(paragraph);
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     let currentIndex = 0;
     const images = document.querySelectorAll('.carousel-inner img');
